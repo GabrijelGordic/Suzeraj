0126a3ce0afd3274b6cf7e3612485a29ac8ddff2
Gabrijel <gabrijel.gordic@gmail.com>
2025-12-09 13:38:54 +0100

    Initial setup: Django project structure and settings

---
+++ backend/config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Djoser Auth URLs
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+]
+
+# This allows us to see images during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
